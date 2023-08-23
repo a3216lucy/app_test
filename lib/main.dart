c04123bec23d4f4a1cca18bfb2d83c8d77fe2ea5
kimi <kimi.lai@tpisoftware.com>
2023-08-23 19:42:46 +0800

    chore: adjust the bottomNavigationBar

--- lib/main.dart
+++ lib/main.dart
@@ -35,6 +35,8 @@ class BottomNavigationBarTool extends StatefulWidget {
 class _MyHomePageState extends State<BottomNavigationBarTool> {
   // 目前選擇頁索引值，預設值為 0
   int _currentIndex = 0;
+  // nav icon 顏色
+  Color? navColor = Colors.grey[400];
   // 頁面列表
   final pages = [
     const HomePage(),
@@ -42,73 +44,70 @@ class _MyHomePageState extends State<BottomNavigationBarTool> {
     const Calendar(),
     const Setting()
   ];
+  // 底部導覽列項目列表
+  final List<dynamic> _navItems = [
+    NavItem(Icons.home, "首頁"),
+    NavItem(Icons.apps, "APP"),
+    NavItem(Icons.date_range, "日曆"),
+    NavItem(Icons.density_medium, "設定"),
+  ];
 
   // BottomNavigationBar 按下處理事件，更新設定當下索引值
   void _onItemClick(int index) {
     setState(() {
       _currentIndex = index;
+      navColor = Colors.white;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
       // 頁面主體
       body: SizedBox(
-          height: MediaQuery.of(context).size.height,
-          child: pages[_currentIndex]),
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        child: pages[_currentIndex],
+      ),
       // 下方的頁面切換欄位
-      bottomNavigationBar: SingleChildScrollView(
-        child: Container(
-            alignment: Alignment.bottomCenter,
-            constraints:
-                const BoxConstraints.tightFor(width: 200.0, height: 60.0),
-            margin: const EdgeInsets.only(left: 10, right: 10, bottom: 25),
-            child: ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(20.0),
-                topRight: Radius.circular(20.0),
-                bottomLeft: Radius.circular(20.0),
-                bottomRight: Radius.circular(20.0),
-              ),
-              child: Theme(
-                data: Theme.of(context).copyWith(
-                  // 設定 BottomNavigationBar 背景色
-                  canvasColor: Color.fromRGBO(78, 78, 97, 0.75),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      // 懸浮在 BottomAppBar上面的中心圓按鈕
+      floatingActionButton: FloatingActionButton(
+        shape: const CircleBorder(),
+        backgroundColor: const Color.fromRGBO(255, 121, 102, 1),
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        // nav 背景顏色
+        color: const Color.fromRGBO(78, 78, 97, 0.75),
+        // 陰影
+        elevation: 0,
+        // 缺口外邊距
+        notchMargin: 10,
+        shape: const CircularNotchedRectangle(),
+        height: 60,
+        child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: _navItems.map((item) {
+              var index = _navItems.indexOf(item);
+              return IconButton(
+                onPressed: () => _onItemClick(index),
+                icon: Icon(
+                  item.icon,
+                  color:
+                      _currentIndex == index ? Colors.white : Colors.grey[400],
                 ),
-                child: BottomNavigationBar(
-                  selectedItemColor: Colors.white,
-                  unselectedItemColor: Colors.grey[400],
-                  type: BottomNavigationBarType.fixed,
-                  showSelectedLabels: false,
-                  showUnselectedLabels: false,
-                  //目前選擇頁索引值
-                  currentIndex: _currentIndex,
-                  //BottomNavigationBar 按下處理事件
-                  onTap: _onItemClick,
-                  items: const <BottomNavigationBarItem>[
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.home),
-                      label: '首頁',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.apps),
-                      label: 'APP',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.date_range),
-                      label: '日曆',
-                    ),
-                    BottomNavigationBarItem(
-                      icon: Icon(Icons.density_medium),
-                      label: '設定',
-                    ),
-                  ],
-                ),
-              ),
-            )),
+              );
+            }).toList()),
       ),
     );
   }
+}
+
+class NavItem {
+  IconData icon;
+  String title;
+
+  NavItem(this.icon, this.title);
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:app_test/components/col_bar.dart';
+import 'package:app_test/components/row_bar.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -16,39 +17,39 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    //   return const Column(
-    //       mainAxisAlignment: MainAxisAlignment.center,
-    //       children: <Widget>[
-    //         SizedBox(
-    //           height: 70,
-    //         ),
-    //         RowBar(),
-    //         SizedBox(
-    //           height: 70,
-    //         ),
-    //         ColBar(),
-    //       ]);
-    // }
-    return DefaultTabController(
-        length: myTabs.length,
-        child: Expanded(
-          child: Column(children: <Widget>[
-            AppBar(
-              backgroundColor: Colors.blueGrey,
-              title: TabBar(
-                tabs: myTabs,
-                isScrollable: true,
-                indicatorColor: Colors.white,
-                labelColor: Colors.white,
-              ),
-            ),
-            Expanded(
-              child: TabBarView(
-                  children: myTabs
-                      .map((Tab tab) => const Center(child: ColBar()))
-                      .toList()),
-            ),
-          ]),
-        ));
+    return const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(
+            height: 70,
+          ),
+          RowBar(),
+          SizedBox(
+            height: 70,
+          ),
+          ColBar(),
+        ]);
   }
+  //   return DefaultTabController(
+  //       length: myTabs.length,
+  //       child: Expanded(
+  //         child: Column(children: <Widget>[
+  //           AppBar(
+  //             backgroundColor: Colors.blueGrey,
+  //             title: TabBar(
+  //               tabs: myTabs,
+  //               isScrollable: true,
+  //               indicatorColor: Colors.white,
+  //               labelColor: Colors.white,
+  //             ),
+  //           ),
+  //           Expanded(
+  //             child: TabBarView(
+  //                 children: myTabs
+  //                     .map((Tab tab) => const Center(child: ColBar()))
+  //                     .toList()),
+  //           ),
+  //         ]),
+  //       ));
+  // }
 }
